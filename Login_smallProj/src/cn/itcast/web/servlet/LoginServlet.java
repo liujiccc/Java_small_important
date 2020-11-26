ee052367b482e6940b737669992115dcb97a5f15
QLBF11 <1170532298+QLBF11@users.noreply.github.com>
2020-11-26 21:15:22 +0800

    servlet文件下载以及解决中文乱码问题

--- Login_smallProj/src/cn/itcast/web/servlet/LoginServlet.java
+++ Login_smallProj/src/cn/itcast/web/servlet/LoginServlet.java
@@ -25,27 +25,28 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //1.设置编码
         request.setCharacterEncoding("utf-8");
-        //2.获取请求参数
-        //这里getParameter都是根据html的name值的
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        //3.封装user对象
-        User loginUser = new User();
-        loginUser.setUsername(username);
-        loginUser.setPassword(password);
-
-
-//        Map<String, String[]> map = request.getParameterMap();
+//        //2.获取请求参数
+//        //这里getParameter都是根据html的name值的
+//        String username = request.getParameter("username");
+//        String password = request.getParameter("password");
 //        //3.封装user对象
 //        User loginUser = new User();
-//        //3.2使用BeanUtils封装
-//        try {
-//            BeanUtils.populate(loginUser,map);
-//        } catch (IllegalAccessException e) {
-//            e.printStackTrace();
-//        } catch (InvocationTargetException e) {
-//            e.printStackTrace();
-//        }
+//        loginUser.setUsername(username);
+//        loginUser.setPassword(password);
+
+
+        //下面代码就是替换上面的
+        Map<String, String[]> map = request.getParameterMap();
+        //3.封装user对象
+        User loginUser = new User();
+        //3.2使用BeanUtils封装
+        try {
+            BeanUtils.populate(loginUser,map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
 
 
         //4.调用UserDao的login方法
