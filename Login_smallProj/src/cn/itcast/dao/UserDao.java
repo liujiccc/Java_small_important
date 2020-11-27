7a387cefd4debed3f51d5a76f461cda6d2f268eb
QLBF11 <56621284+QLBF11@users.noreply.github.com>
2020-11-27 15:58:37 +0800

    Update UserDao.java

--- Login_smallProj/src/cn/itcast/dao/UserDao.java
+++ Login_smallProj/src/cn/itcast/dao/UserDao.java
@@ -22,7 +22,7 @@ public class UserDao {
     /**
      * 登录方法
      * @param loginUser 只有用户名和密码
-     * @return user包含用户全部数据,没有查询到，返回null
+     * @return user包含用户全部数据,没有查询到，返回null给loginservlet进行逻辑处理
      */
     public User login(User loginUser){
         try {
